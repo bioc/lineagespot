4383432213da8085ba97fd38a6a9372e28b4cb7a
Sofoklis Keisaris <89997077+Sofokli5@users.noreply.github.com>
2021-12-20 15:11:33 +0200

    Update merge_vcf.R
    "=" substituted by "<-" when needed

--- R/merge_vcf.R
+++ R/merge_vcf.R
@@ -48,7 +48,7 @@ merge_vcf <- function(vcf_fls = NULL,
 
     if( is.null(vcf_fls) ) {
 
-        vcf_fls = list.files(vcf_folder,
+        vcf_fls <- list.files(vcf_folder,
         pattern = "vcf",
         full.names = TRUE)
 
@@ -64,13 +64,13 @@ merge_vcf <- function(vcf_fls = NULL,
 
     # Read input VCF files -----------------------------------------------------
 
-    vcf_list = base::list()
+    vcf_list <- base::list()
 
     for(i in vcf_fls) {
 
         # vcf_list[[i]] = vcfR::read.vcfR(i, verbose = FALSE)
         
-        vcf_list[[i]] = readVcf(i)
+        vcf_list[[i]] <- readVcf(i)
 
     }
 
@@ -79,42 +79,42 @@ merge_vcf <- function(vcf_fls = NULL,
 
     # Convert VCF to table -----------------------------------------------------
 
-    vcf_list = base::lapply(vcf_list, vcf_to_table)
+    vcf_list <- base::lapply(vcf_list, vcf_to_table)
 
     # break multiple rules -----------------------------------------------------
 
-    vcf_list = base::lapply(vcf_list, break_multiple_variants)
+    vcf_list <- base::lapply(vcf_list, break_multiple_variants)
 
     # add variant parameters ---------------------------------------------------
 
-    vcf_list = base::lapply(vcf_list, compute_AF)
+    vcf_list <- base::lapply(vcf_list, compute_AF)
 
     # add variant parameters ---------------------------------------------------
 
-    vcf_list = base::lapply(vcf_list, change_AA_abbreviations)
+    vcf_list <- base::lapply(vcf_list, change_AA_abbreviations)
 
     # add variant parameters ---------------------------------------------------
 
-    genes = read_gene_coordinates(gff_path = gff3_path)
+    genes <- read_gene_coordinates(gff_path = gff3_path)
 
-    vcf_list = base::lapply(vcf_list, correct_Orf1ab_gene, genes)
+    vcf_list <- base::lapply(vcf_list, correct_Orf1ab_gene, genes)
 
     # add sample name ----------------------------------------------------------
 
     for(i in names(vcf_list)) {
 
-        sample_name = base::unlist(str_split(i, "\\/"))
-        sample_name = sample_name[length(sample_name)]
+        sample_name <- base::unlist(str_split(i, "\\/"))
+        sample_name <- sample_name[length(sample_name)]
 
-        sample_name = str_split(sample_name, "\\.vcf", simplify = TRUE)[,1]
+        sample_name <- str_split(sample_name, "\\.vcf", simplify = TRUE)[,1]
 
-        vcf_list[[i]]$sample = sample_name
+        vcf_list[[i]]$sample <- sample_name
 
     }
 
     # Combine all vcfs into one table ------------------------------------------
 
-    vcf_list = rbindlist(vcf_list)
+    vcf_list <- rbindlist(vcf_list)
 
     return(vcf_list)
 
@@ -122,31 +122,31 @@ merge_vcf <- function(vcf_fls = NULL,
 
 vcf_to_table <- function(x) {
 
-    # out = cbind(x@fix[, seq_len(7)],
+    # out <- cbind(x@fix[, seq_len(7)],
     # vcfR::extract_gt_tidy(x, verbose = FALSE),
     # vcfR::extract_info_tidy(x))
     
-    out = cbind(as.data.frame(rowRanges(x)),
+    out <- cbind(as.data.frame(rowRanges(x)),
                 as.data.frame(fixed(x)),
                 as.data.frame(info(x)))
     
-    out$ALT = unlist(lapply(out$ALT, function(x){ return(paste(as.character(x), collapse = ",")) }))
+    out$ALT <- unlist(lapply(out$ALT, function(x){ return(paste(as.character(x), collapse = ",")) }))
     
-    out$ANN = unlist(lapply(out$ANN, function(x){ return(x[1]) }))
+    out$ANN <- unlist(lapply(out$ANN, function(x){ return(x[1]) }))
     
-    out$ID = row.names(out)
+    out$ID <- row.names(out)
     
-    out$AD = unlist(lapply(assays(x)[["AD"]], function(x) {return(paste(x, collapse = ","))}))
+    out$AD <- unlist(lapply(assays(x)[["AD"]], function(x) {return(paste(x, collapse = ","))}))
 
-    out = setDT(out)
+    out <- setDT(out)
 
-    out = out[which(out$ALT != "<*>"), ]
+    out <- out[which(out$ALT != "<*>"), ]
 
-    ANN_matrix = str_split(out$ANN, "\\|", simplify = TRUE)[,c(4, 10, 11)]
+    ANN_matrix <- str_split(out$ANN, "\\|", simplify = TRUE)[,c(4, 10, 11)]
 
-    # ANN_matrix = paste0(ANN_matrix[,1], "|", ANN_matrix[,2])
+    # ANN_matrix <- paste0(ANN_matrix[,1], "|", ANN_matrix[,2])
 
-    values_wewant = c("seqnames", # "CHROM",
+    values_wewant <- c("seqnames", # "CHROM",
                       "start", # "POS",
                       "ID",
                       "REF",
@@ -154,9 +154,9 @@ vcf_to_table <- function(x) {
                       "DP",  # "gt_DP",
                       "AD" ) # "gt_AD")
 
-    result = intersect(values_wewant, colnames(out))
+    result <- intersect(values_wewant, colnames(out))
 
-    out = out[, result, with = FALSE]
+    out <- out[, result, with = FALSE]
 
     if(identical((intersect(result, "gt_DP")), "gt_DP")){
 
@@ -170,12 +170,12 @@ vcf_to_table <- function(x) {
 
     }
 
-    colnames(out) = c("CHROM", "POS", "ID", "REF", "ALT", "DP", "AD")
+    colnames(out) <- c("CHROM", "POS", "ID", "REF", "ALT", "DP", "AD")
 
-    out$CHROM = str_squish(out$CHROM)
-    out$Gene_Name = ANN_matrix[,1]
-    out$Nt_alt = ANN_matrix[,2]
-    out$AA_alt = ANN_matrix[,3]
+    out$CHROM <- str_squish(out$CHROM)
+    out$Gene_Name <- ANN_matrix[,1]
+    out$Nt_alt <- ANN_matrix[,2]
+    out$AA_alt <- ANN_matrix[,3]
 
     return(out)
 
@@ -183,17 +183,17 @@ vcf_to_table <- function(x) {
 
 break_multiple_variants <- function(x) {
 
-    ALT_matrix = str_split(x$ALT, ",", simplify = TRUE)
-    AD_matrix = str_split(x$AD, ",", simplify = TRUE)
+    ALT_matrix <- str_split(x$ALT, ",", simplify = TRUE)
+    AD_matrix <- str_split(x$AD, ",", simplify = TRUE)
 
-    out = list()
+    out <- list()
 
     for(i in seq_len(ncol(ALT_matrix))) {
 
-        who = which(ALT_matrix[,i] != "")
+        who <- which(ALT_matrix[,i] != "")
 
 
-        out[[i]] = data.table(CHROM = x[who, ]$CHROM,
+        out[[i]] <- data.table(CHROM = x[who, ]$CHROM,
                               POS = x[who, ]$POS,
                               ID = x[who, ]$ID,
                               REF = x[who, ]$REF,
@@ -207,11 +207,11 @@ break_multiple_variants <- function(x) {
 
     }
 
-    out = rbindlist(out)
+    out <- rbindlist(out)
 
-    out$POS = as.numeric(out$POS)
+    out$POS <- as.numeric(out$POS)
 
-    out = out[order(out$POS), ]
+    out <- out[order(out$POS), ]
 
     return(out)
 
@@ -219,12 +219,12 @@ break_multiple_variants <- function(x) {
 
 compute_AF <- function(x) {
 
-    x$AD_ref = as.numeric(x$AD_ref)
-    x$AD_alt = as.numeric(x$AD_alt)
+    x$AD_ref <- as.numeric(x$AD_ref)
+    x$AD_alt <- as.numeric(x$AD_alt)
 
-    x$AF = x$AD_alt / x$DP
+    x$AF <- x$AD_alt / x$DP
 
-    x$ID = paste(x$CHROM, x$POS, x$REF, x$ALT, sep = ";")
+    x$ID <- paste(x$CHROM, x$POS, x$REF, x$ALT, sep = ";")
 
     return(x)
 
@@ -232,7 +232,7 @@ compute_AF <- function(x) {
 
 change_AA_abbreviations <- function(x) {
 
-    AA_abbreviations = data.table(Three_Letter = c("Ala",
+    AA_abbreviations <- data.table(Three_Letter <- c("Ala",
                                                    "Arg",
                                                    "Asn",
                                                    "Asp",
@@ -253,7 +253,7 @@ change_AA_abbreviations <- function(x) {
                                                    "Tyr",
                                                    "Val"),
 
-                                  One_Letter = c("A",
+                                  One_Letter <- c("A",
                                                  "R",
                                                  "N",
                                                  "D",
@@ -275,12 +275,12 @@ change_AA_abbreviations <- function(x) {
                                                  "V"))
 
 
-    x$Nt_alt = str_remove_all(x$Nt_alt, "c\\.")
-    x$AA_alt = str_remove_all(x$AA_alt, "p\\.")
+    x$Nt_alt <- str_remove_all(x$Nt_alt, "c\\.")
+    x$AA_alt <- str_remove_all(x$AA_alt, "p\\.")
 
     for(i in seq_len(nrow(AA_abbreviations))) {
 
-        x$AA_alt = str_replace_all(x$AA_alt,
+        x$AA_alt <- str_replace_all(x$AA_alt,
         AA_abbreviations[i,]$Three_Letter,
         AA_abbreviations[i,]$One_Letter)
 
@@ -294,20 +294,20 @@ correct_Orf1ab_gene <- function(x, genes) {
 
     x$codon_num = 0
 
-    x = x[order(x$POS), ]
+    x <- x[order(x$POS), ]
 
     for(i in seq_len(nrow(genes))) {
 
-        who = which(x$POS >= genes[i,]$start_pos & x$POS <= genes[i,]$end_pos)
+        who <- which(x$POS >= genes[i,]$start_pos & x$POS <= genes[i,]$end_pos)
 
-        x[who, ]$codon_num = x[who, ]$POS - genes[i,]$start_pos + 1
-        x[who, ]$Gene_Name = genes[i,]$gene_name
-        x[who, ]$codon_num = ( x[who, ]$codon_num %/% 3 ) +
+        x[who, ]$codon_num <- x[who, ]$POS - genes[i,]$start_pos + 1
+        x[who, ]$Gene_Name <- genes[i,]$gene_name
+        x[who, ]$codon_num <- ( x[who, ]$codon_num %/% 3 ) +
                              ( x[who, ]$codon_num %% 3 > 0 )
 
     }
 
-    x = x[which(x$codon_num != 0), ]
+    x <- x[which(x$codon_num != 0), ]
 
     return(x)
 
@@ -315,20 +315,20 @@ correct_Orf1ab_gene <- function(x, genes) {
 
 read_gene_coordinates <- function(gff_path) {
 
-    gene_annot = fread(gff_path, header = FALSE, sep = "\t")
+    gene_annot - fread(gff_path, header = FALSE, sep = "\t")
 
-    gene_annot = gene_annot[, c(4, 5, 9), with = FALSE]
+    gene_annot <- gene_annot[, c(4, 5, 9), with = FALSE]
 
-    colnames(gene_annot) = c("start_pos", "end_pos", "gene_name")
+    colnames(gene_annot) <- c("start_pos", "end_pos", "gene_name")
 
-    gene_annot[which(gene_annot$gene_name %in% paste0("NSP", seq_len(10))), ]$gene_name = "ORF1a"
+    gene_annot[which(gene_annot$gene_name %in% paste0("NSP", seq_len(10))), ]$gene_name <- "ORF1a"
 
-    gene_annot[which(str_detect(gene_annot$gene_name, "NSP")), ]$gene_name = "ORF1b"
+    gene_annot[which(str_detect(gene_annot$gene_name, "NSP")), ]$gene_name <- "ORF1b"
 
-    gene_annot = gene_annot[, .(start_pos = min(start_pos),
+    gene_annot <- gene_annot[, .(start_pos = min(start_pos),
     end_pos = max(end_pos)), by = gene_name]
 
-    gene_annot = gene_annot[order(gene_annot$start_pos), ]
+    gene_annot <- gene_annot[order(gene_annot$start_pos), ]
 
     return(gene_annot)
 
