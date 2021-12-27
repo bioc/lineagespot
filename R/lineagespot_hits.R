39dbbb2b7bf877931551e102245d5a889487f59b
nikopech <nikosp41@gmail.com>
2021-12-27 15:08:23 +0200

    spaces, 80 characters

--- R/lineagespot_hits.R
+++ R/lineagespot_hits.R
@@ -26,38 +26,42 @@
 #'
 #' @examples
 #'
-#' variants_table <- merge_vcf(vcf_folder = system.file("extdata",
-#'                                                  "vcf-files",
-#'                                                  package = "lineagespot"),
-#'
-#'                              gff3_path = system.file("extdata",
-#'                                                  "NC_045512.2_annot.gff3",
-#'                                                  package = "lineagespot"))
+#' variants_table <- merge_vcf(
+#'     vcf_folder = system.file("extdata",
+#'         "vcf-files",
+#'         package = "lineagespot"
+#'     ),
+#'     gff3_path = system.file("extdata",
+#'         "NC_045512.2_annot.gff3",
+#'         package = "lineagespot"
+#'     )
+#' )
 #'
 #' # retrieve lineage reports using outbreak.info's API
 #'
-#' lineage_hits_table <- lineagespot_hits(vcf_table = variants_table,
-#'                                       voc = c("B.1.1.7", "B.1.617.2"))
+#' lineage_hits_table <- lineagespot_hits(
+#'     vcf_table = variants_table,
+#'     voc = c("B.1.1.7", "B.1.617.2")
+#' )
 #'
 #'
 #' # use user-specified references
-#' lineage_hits_table.2 <-lineagespot_hits(vcf_table = variants_table,
-#'                                  ref_folder = system.file("extdata", "ref",
-#'                                  package = "lineagespot"))
-#'
-
-
-lineagespot_hits <- function(vcf_table = NULL,
-                            ref_folder = NULL,
-                            voc = c("B.1.617.2", "B.1.1.7", "B.1.351", "P.1"),
-                            file.out = paste0("lineage_hits_",
-                                                Sys.Date(), ".txt")) {
-
-
-    if( is.null(vcf_table) ) {
-
-        stop('Please provide a tab-delimited table containing variants.')
-
+#' lineage_hits_table.2 <- lineagespot_hits(
+#'     vcf_table = variants_table,
+#'     ref_folder = system.file("extdata", "ref",
+#'         package = "lineagespot"
+#'     )
+#' )
+lineagespot_hits <- function(
+    vcf_table = NULL,
+    ref_folder = NULL,
+    voc = c("B.1.617.2", "B.1.1.7", "B.1.351", "P.1"),
+    file.out = paste0(
+        "lineage_hits_", Sys.Date(), ".txt"
+    )
+) {
+    if (is.null(vcf_table)) {
+        stop("Please provide a tab-delimited table containing variants.")
     }
 
     # clean AA variants ------------------------------------------------------
@@ -66,35 +70,33 @@ lineagespot_hits <- function(vcf_table = NULL,
     codon_num <- str_split(vcf_table$AA_alt, "[A-Z]|[a-z]|\\_|\\*|\\?")
 
     aa_variants <- lapply(aa_variants, function(x) {
-
-        return( x[x != ""] )
-
+        return(x[x != ""])
     })
 
     codon_num <- lapply(codon_num, function(x) {
-
-        return( x[x != ""] )
-
+        return(x[x != ""])
     })
 
 
     # merge overall tables ----------------------------------------------------
 
     aa_split_list <- lapply(seq_len(nrow(vcf_table)), function(i) {
+        out <- data.table(
+            ref_aa = NA,
+            alt_aa = NA,
+            info = NA,
+            codon_num = NA,
+            codon_start = NA,
+            codon_end = NA
+        )
 
-        out <- data.table(ref_aa = NA, alt_aa = NA, info = NA,
-                        codon_num = NA, codon_start = NA, codon_end = NA)
 
-
-        if(length(aa_variants[[i]]) == 2) {
-
+        if (length(aa_variants[[i]]) == 2) {
             out$ref_aa <- aa_variants[[i]][1]
             out$alt_aa <- aa_variants[[i]][2]
 
             out$codon_num <- codon_num[[i]][1]
-
-        } else if(length(aa_variants[[i]] == 3)) {
-
+        } else if (length(aa_variants[[i]] == 3)) {
             out$ref_aa <- aa_variants[[i]][1]
             out$alt_aa <- aa_variants[[i]][2]
 
@@ -102,25 +104,29 @@ lineagespot_hits <- function(vcf_table = NULL,
 
             out$codon_start <- codon_num[[i]][1]
             out$codon_end <- codon_num[[i]][2]
-
         } else {
 
 
         }
 
         return(out)
-
     })
 
     aa_split_list <- rbindlist(aa_split_list)
 
-    aa_split_list <- aa_split_list[, c("ref_aa", "alt_aa", "info",
-                                    "codon_start","codon_end"), with = FALSE]
+    aa_split_list <- aa_split_list[, c(
+        "ref_aa",
+        "alt_aa",
+        "info",
+        "codon_start",
+        "codon_end"
+    ), with = FALSE]
 
     vcf_table <- cbind(vcf_table, aa_split_list)
 
-    vcf_table$change_length_nt <- abs(str_length(vcf_table$ALT) -
-                                        str_length(vcf_table$REF))
+    vcf_table$change_length_nt <- abs(
+        str_length(vcf_table$ALT) - str_length(vcf_table$REF)
+    )
 
     vcf_table$codon_num <- as.numeric(vcf_table$codon_num)
     vcf_table$codon_start <- as.numeric(vcf_table$codon_start)
@@ -129,16 +135,16 @@ lineagespot_hits <- function(vcf_table = NULL,
 
     # read reference ---------------------------------------------------------
 
-    if(!is.null(ref_folder)) {
-
-        fls_ref <- list.files(ref_folder,
-        pattern = "txt",
-        full.names = TRUE)
+    if (!is.null(ref_folder)) {
+        fls_ref <- list.files(
+            ref_folder,
+            pattern = "txt",
+            full.names = TRUE
+        )
 
         reference_list <- list()
 
-        for(i in fls_ref){
-
+        for (i in fls_ref) {
             ref <- fread(i)
 
             ref <- ref[order(ref$gene), ]
@@ -147,89 +153,75 @@ lineagespot_hits <- function(vcf_table = NULL,
 
             ref$type <- "snp"
 
-            ref[which(str_detect(ref$`amino acid`, "del")),]$type = "deletion"
+            ref[which(str_detect(ref$`amino acid`, "del")), ]$type <- "deletion"
 
             reference_list[[i]] <- ref
-
         }
-
     } else {
-
         reference_list <- get_lineage_report(lineages = voc)
-
     }
 
     VoC_hits_list <- base::list()
 
-    for(ref_index in names(reference_list)) {
-
+    for (ref_index in names(reference_list)) {
         ref <- reference_list[[ref_index]]
 
         aa_variants <- str_split(ref$`amino acid`, "[0-9]|_|\\/")
-        codon_num <- str_split(ref$`amino acid`,"[A-Z]|[a-z]|\\_|\\*|\\?|\\/")
+        codon_num <- str_split(ref$`amino acid`, "[A-Z]|[a-z]|\\_|\\*|\\?|\\/")
 
         aa_variants <- lapply(aa_variants, function(x) {
+            x <- x[x != ""]
+            x <- str_replace_all(x, "\\*", "\\\\*")
 
-        x <- x[x != ""]
-
-        x <- str_replace_all(x, "\\*", "\\\\*")
-
-            return( x )
-
+            return(x)
         })
 
         codon_num <- lapply(codon_num, function(x) {
-
-            return( as.numeric(x[x != ""]) )
-
+            return(as.numeric(x[x != ""]))
         })
 
         # VoC hits ------------------------------------------------------------
 
         voc_data <- list()
 
-        for(i in seq_len(nrow(ref))) {
+        for (i in seq_len(nrow(ref))) {
+            if (ref[i, ]$type == "snp") {
+                temp <- vcf_table[which(vcf_table$Gene_Name == ref[i, ]$gene), ]
 
-            if(ref[i,]$type == "snp") {
+                temp <- temp[which(
+                    str_detect(temp$ref_aa, aa_variants[[i]][1])
+                ), ]
 
-                temp <- vcf_table[which(vcf_table$Gene_Name == ref[i,]$gene), ]
+                temp <- temp[which(
+                    str_detect(temp$alt_aa, aa_variants[[i]][2])
+                ), ]
 
-                temp <- temp[which(str_detect(temp$ref_aa,
-                                             aa_variants[[i]][1] )), ]
-
-                temp <- temp[which(str_detect(temp$alt_aa,
-                                             aa_variants[[i]][2])), ]
-
-                who <- which(temp$codon_num == codon_num[[i]] |
-                                temp$codon_num == codon_num[[i]] + 1 |
-                                temp$codon_num == codon_num[[i]] - 1)
+                who <- which(
+                    temp$codon_num == codon_num[[i]] |
+                        temp$codon_num == codon_num[[i]] + 1 |
+                        temp$codon_num == codon_num[[i]] - 1
+                )
 
                 temp <- temp[who, ]
-
             } else {
-
-                temp <- vcf_table[which(vcf_table$Gene_Name == ref[i,]$gene), ]
+                temp <- vcf_table[which(vcf_table$Gene_Name == ref[i, ]$gene), ]
 
                 temp <- temp[which(str_detect(temp$AA_alt, "del")), ]
 
-                who <- which(temp$codon_start == codon_num[[i]][1] |
-                                temp$codon_start == codon_num[[i]][2])
+                who <- which(
+                    temp$codon_start == codon_num[[i]][1] |
+                        temp$codon_start == codon_num[[i]][2]
+                )
 
                 temp <- temp[who, ]
 
-                if(nrow(temp) != 0) {
-
+                if (nrow(temp) != 0) {
                     temp$AA_alt <- ref[i, ]$`amino acid`
-
                 }
-
             }
 
 
-            voc_data[[ref[i,]$barcode]] <- temp
-
-
-
+            voc_data[[ref[i, ]$barcode]] <- temp
         }
 
         not_overlapping_variants <- names(which(lapply(voc_data, nrow) == 0))
@@ -243,20 +235,24 @@ lineagespot_hits <- function(vcf_table = NULL,
 
         # collapse table ------------------------------------------------------
 
-        voc_data <- voc_data[, c("POS",
-                                "DP",
-                                "AD_alt",
-                                "Gene_Name",
-                                "AA_alt",
-                                "sample"), with = FALSE]
+        voc_data <- voc_data[, c(
+            "POS",
+            "DP",
+            "AD_alt",
+            "Gene_Name",
+            "AA_alt",
+            "sample"
+        ), with = FALSE]
 
-        voc_data <- voc_data[, .(DP = unique(DP),
-                                AD_alt = sum(AD_alt)),
-                            by = .(POS, Gene_Name, AA_alt, sample)]
+        voc_data <- voc_data[, .(
+            DP = unique(DP),
+            AD_alt = sum(AD_alt)
+        ), by = .(POS, Gene_Name, AA_alt, sample)]
 
-        voc_data <- voc_data[, .(DP = sum(DP),
-                                AD_alt = sum(AD_alt)),
-                            by = .(Gene_Name, AA_alt, sample)]
+        voc_data <- voc_data[, .(
+            DP = sum(DP),
+            AD_alt = sum(AD_alt)
+        ), by = .(Gene_Name, AA_alt, sample)]
 
 
         voc_data$AF <- voc_data$AD_alt / voc_data$DP
@@ -264,42 +260,42 @@ lineagespot_hits <- function(vcf_table = NULL,
         # add non overlapping rules -------------------------------------------
 
 
-        not_overlapping_variants <- str_split(not_overlapping_variants,
-                                            "\\:", simplify = TRUE)
+        not_overlapping_variants <- str_split(
+            not_overlapping_variants, "\\:",
+            simplify = TRUE
+        )
 
-        voc_data <- rbind(voc_data,
-                    data.table(Gene_Name = rep(not_overlapping_variants[,1],
-                                        length(unique(voc_data$sample))),
-
-                                    AA_alt = rep(not_overlapping_variants[,2],
-                                            length(unique(voc_data$sample))),
-
-                                    sample = unique(voc_data$sample),
-
-                                    DP = 0,
-
-                                    AD_alt = 0,
-
-                                    AF = 0))
+        voc_data <- rbind(
+            voc_data,
+            data.table(
+                Gene_Name = rep(
+                    not_overlapping_variants[, 1],
+                    length(unique(voc_data$sample))
+                ),
+                AA_alt = rep(
+                    not_overlapping_variants[, 2],
+                    length(unique(voc_data$sample))
+                ),
+                sample = unique(voc_data$sample),
+                DP = 0,
+                AD_alt = 0,
+                AF = 0
+            )
+        )
 
         VoC_hits_list[[ref_index]] <- voc_data
-
     }
 
-    for(i in base::names(VoC_hits_list)) {
-
+    for (i in base::names(VoC_hits_list)) {
         strain <- str_split(i, "\\/", simplify = TRUE)
         strain <- strain[, ncol(strain)]
         strain <- str_remove(strain, "\\.txt")
 
         VoC_hits_list[[i]]$lineage <- strain
-
     }
 
     voc_data <- rbindlist(VoC_hits_list)
 
 
     return(voc_data)
-
 }
-
--- R/uniq_variants.R
+++ R/uniq_variants.R
@@ -11,7 +11,7 @@
 #'
 #' @param AF_threshold
 #' A parameter indicating the AF threshold that is going to applied in order to
-#'identify the presence or not of a variant. This is used to compute the number
+#' identify the presence or not of a variant. This is used to compute the number
 #' of variants in a sample and eventually the proportion of a lineage.
 #'
 #' @param file.out
@@ -25,43 +25,49 @@
 #'
 #' @examples
 #'
-#' variants_table <- merge_vcf(vcf_folder = system.file("extdata", "vcf-files",
-#'                                                  package = "lineagespot"),
+#' variants_table <- merge_vcf(
+#'     vcf_folder = system.file("extdata", "vcf-files",
+#'         package = "lineagespot"
+#'     ),
+#'     gff3_path = system.file("extdata",
+#'         "NC_045512.2_annot.gff3",
+#'         package = "lineagespot"
+#'     )
+#' )
 #'
-#'                              gff3_path = system.file("extdata",
-#'                                                  "NC_045512.2_annot.gff3",
-#'                                                  package = "lineagespot"))
-#'
-#' lineage_hits_table <- lineagespot_hits(vcf_table = variants_table,
-#'                                      voc = c("B.1.1.7", "B.1.617.2"))
+#' lineage_hits_table <- lineagespot_hits(
+#'     vcf_table = variants_table,
+#'     voc = c("B.1.1.7", "B.1.617.2")
+#' )
 #'
 #' report <- uniq_variants(hits_table = lineage_hits_table)
 #' head(report)
-
-
-uniq_variants <- function(hits_table = NULL,
-                          AF_threshold = 0.8,
-                          file.out = paste0("lineage_report_",
-                                            Sys.Date(), ".txt")) {
-
-    if( is.null(hits_table) ) {
-
-    stop('Please provide a tab-delimited table containing SARS-CoV-2 overlaps')
-
+uniq_variants <- function(
+    hits_table = NULL,
+    AF_threshold = 0.8,
+    file.out = paste0(
+        "lineage_report_", Sys.Date(), ".txt"
+    )
+) {
+    if (is.null(hits_table)) {
+        stop("Please provide a tab-delimited table")
     }
 
     # lineage overview -------------------------------------------------------
 
-    lineage_stats <- unique(hits_table[, c("Gene_Name", "AA_alt", "lineage"),
-                                        with = FALSE])
+    lineage_stats <- unique(
+        hits_table[, c("Gene_Name", "AA_alt", "lineage"), with = FALSE]
+    )
 
     lineage_stats <- lineage_stats[, .N, by = lineage]
 
     count_lineages <- hits_table[which(hits_table$AF >= AF_threshold), ]
 
-    count_lineages <- unique(count_lineages[, c("Gene_Name", "AA_alt",
-                                                "sample", "lineage"),
-                                                with = FALSE])
+    count_lineages <- unique(
+        count_lineages[, c(
+            "Gene_Name", "AA_alt", "sample", "lineage"
+        ), with = FALSE]
+    )
 
     count_lineages <- count_lineages[, .N, by = .(lineage, sample)]
 
@@ -72,7 +78,10 @@ uniq_variants <- function(hits_table = NULL,
 
     # Find unique variants per lineage ---------------------------------------
 
-    count_uniq <- hits_table[,c("Gene_Name", "AA_alt", "lineage"),with = FALSE]
+    count_uniq <- hits_table[, c(
+        "Gene_Name", "AA_alt", "lineage"
+    ), with = FALSE]
+    
     count_uniq <- unique(count_uniq)
     count_uniq <- count_uniq[order(count_uniq$Gene_Name, count_uniq$AA_alt), ]
     count_uniq <- count_uniq[, .N, by = .(Gene_Name, AA_alt)]
@@ -80,15 +89,16 @@ uniq_variants <- function(hits_table = NULL,
 
     count_uniq <- count_uniq[which(count_uniq$N == 1), ]
 
-    hits_table_uniq <- hits_table[which(hits_table$Gene_Name %in% 
-                                        count_uniq$Gene_Name &
-                                        hits_table$AA_alt %in% 
-                                        count_uniq$AA_alt), ]
+    hits_table_uniq <- hits_table[which(
+        hits_table$Gene_Name %in% count_uniq$Gene_Name &
+            hits_table$AA_alt %in% count_uniq$AA_alt
+    ), ]
 
     # mean AF of unique variants ---------------------------------------------
 
-    meanAF_uniq <- hits_table_uniq[, .(meanAF_uniq = mean(AF)),
-                                   by = .(lineage, sample)]
+    meanAF_uniq <- hits_table_uniq[, .(
+        meanAF_uniq = mean(AF)
+    ), by = .(lineage, sample)]
 
     # filter out zero-AF variants --------------------------------------------
 
@@ -96,21 +106,26 @@ uniq_variants <- function(hits_table = NULL,
 
     # min AF of non zeror unique variants
 
-    minAF_uniq <- hits_table_uniq[, .(minAF_uniq_nonzero = min(AF)),
-                                   by = .(lineage, sample)]
+    minAF_uniq <- hits_table_uniq[, .(
+        minAF_uniq_nonzero = min(AF)
+    ), by = .(lineage, sample)]
 
     # create output table ----------------------------------------------------
 
-    overall <- rbindlist(list(meanAF, meanAF_uniq, minAF_uniq, count_lineages),
-    use.names = TRUE,
-    fill = TRUE)
+    overall <- rbindlist(
+        list(meanAF, meanAF_uniq, minAF_uniq, count_lineages),
+        use.names <- TRUE,
+        fill <- TRUE
+    )
 
     overall <- overall[order(overall$lineage, overall$sample), ]
 
-    overall <- overall[, lapply(.SD, sum, na.rm = TRUE), 
-                        by = .(lineage, sample)]
+    overall <- overall[, lapply(
+        .SD, sum,
+        na.rm = TRUE
+    ), by = .(lineage, sample)]
 
-    overall[which(overall$minAF_uniq_nonzero == 0), ]$minAF_uniq_nonzero = NA
+    overall[which(overall$minAF_uniq_nonzero == 0), ]$minAF_uniq_nonzero <- NA
 
     who <- base::match(overall$lineage, lineage_stats$lineage)
 
@@ -119,10 +134,4 @@ uniq_variants <- function(hits_table = NULL,
     overall$`lineage prop.` <- overall$N / overall$`lineage N. rules`
 
     return(overall)
-
-
-
 }
-
-
-
